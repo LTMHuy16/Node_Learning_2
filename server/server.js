0c7bf14569cde10768d8321f18bb5a3a275c2a5e
Hung Dinh <hung.dinh@junoecommerce.com>
2023-07-18 22:02:22 +0700

    setup project base

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const dbConnect = require("./config/dbconnect");
+const initRoutes = require("./routes");
+const cookieParser = require("cookie-parser");
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// CONFIG MIDDLEWARE
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// START SERVICES
+dbConnect();
+initRoutes(app);
+
+// START SERVER
+app.listen(port, () => {
+  console.log("Server running on the port: " + port);
+});
